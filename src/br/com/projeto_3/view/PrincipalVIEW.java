8fa1916b1cc98c87f11894938170b3e42a03d3ce
JesseV22 <starkjesse2020@gmail.com>
2023-04-19 11:22:00 -0300

    up

---
+++ src/br/com/projeto_3/view/PrincipalVIEW.java
@@ -0,0 +1,208 @@
+package br.com.projeto_3.view;
+
+import javax.swing.JOptionPane;
+import java.awt.Image;
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author André Luis Gobbi Primo / Ivan Oliveira Lopes
+ */
+public class PrincipalVIEW extends javax.swing.JFrame {
+
+    /**
+     * Creates new form PrincipalVIEW
+     */
+    public PrincipalVIEW() {
+        initComponents();
+        this.setLocationRelativeTo(null);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        ImageIcon imageicon = new ImageIcon(getClass().getResource("imagens/tela_inicial.jpg"));
+        Image image = imageicon.getImage();
+
+        desktopPane = new javax.swing.JDesktopPane(){
+            public void paintComponent(Graphics graphics){
+                graphics.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+            }
+        }
+
+        ;
+        menuBar = new javax.swing.JMenuBar();
+        menuCadastro = new javax.swing.JMenu();
+        itemMenuFornecedor = new javax.swing.JMenuItem();
+        itemMenuProduto = new javax.swing.JMenuItem();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        menuVenda = new javax.swing.JMenu();
+        jMenuItem2 = new javax.swing.JMenuItem();
+        menuSair = new javax.swing.JMenu();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        menuCadastro.setMnemonic('c');
+        menuCadastro.setText("Cadastro");
+
+        itemMenuFornecedor.setMnemonic('f');
+        itemMenuFornecedor.setText("Fornecedor");
+        itemMenuFornecedor.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                itemMenuFornecedorActionPerformed(evt);
+            }
+        });
+        menuCadastro.add(itemMenuFornecedor);
+
+        itemMenuProduto.setMnemonic('p');
+        itemMenuProduto.setText("Produto");
+        itemMenuProduto.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                itemMenuProdutoActionPerformed(evt);
+            }
+        });
+        menuCadastro.add(itemMenuProduto);
+
+        jMenuItem1.setText("Cliente");
+        menuCadastro.add(jMenuItem1);
+
+        menuBar.add(menuCadastro);
+
+        menuVenda.setText("Venda");
+
+        jMenuItem2.setText("Realizar Venda");
+        menuVenda.add(jMenuItem2);
+
+        menuBar.add(menuVenda);
+
+        menuSair.setMnemonic('s');
+        menuSair.setText("Sair");
+        menuSair.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                menuSairMouseClicked(evt);
+            }
+        });
+        menuBar.add(menuSair);
+
+        setJMenuBar(menuBar);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(desktopPane, javax.swing.GroupLayout.PREFERRED_SIZE, 885, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(desktopPane, javax.swing.GroupLayout.PREFERRED_SIZE, 483, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void menuSairMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_menuSairMouseClicked
+        sair();
+    }//GEN-LAST:event_menuSairMouseClicked
+
+    private void itemMenuFornecedorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_itemMenuFornecedorActionPerformed
+        abreFornecedorVIEW();
+    }//GEN-LAST:event_itemMenuFornecedorActionPerformed
+
+    private void itemMenuProdutoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_itemMenuProdutoActionPerformed
+        abreProdutoVIEW();
+    }//GEN-LAST:event_itemMenuProdutoActionPerformed
+
+    /**
+     * Método para fechar o sistema.
+     */ 
+    private void sair(){
+        Object[] options = { "Sair", "Cancelar" };
+        if(JOptionPane.showOptionDialog(null, "Deseja Sair do Sistema", "Informação", 
+                JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE, null, options, options[0]) == 0){
+            System.exit(0);
+        } 
+    } 
+    
+    /**
+     * Método para abrir a classe FornecedorVIEW.
+     */
+    private void abreFornecedorVIEW(){
+        FornecedorVIEW fornecedorVIEW = new FornecedorVIEW();
+        this.desktopPane.add(fornecedorVIEW);
+        fornecedorVIEW.setVisible(true); 
+        fornecedorVIEW.setPosicao();
+    }
+    
+    
+    /**
+     * Método para abrir a classe ProdutoVIEW.
+     */
+    private void abreProdutoVIEW(){
+        ProdutoVIEW produtoVIEW = new ProdutoVIEW();
+        this.desktopPane.add(produtoVIEW);
+        produtoVIEW.setVisible(true); 
+        produtoVIEW.setPosicao();
+    }
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(PrincipalVIEW.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(PrincipalVIEW.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(PrincipalVIEW.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(PrincipalVIEW.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new PrincipalVIEW().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JDesktopPane desktopPane;
+    private javax.swing.JMenuItem itemMenuFornecedor;
+    private javax.swing.JMenuItem itemMenuProduto;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuBar menuBar;
+    private javax.swing.JMenu menuCadastro;
+    private javax.swing.JMenu menuSair;
+    private javax.swing.JMenu menuVenda;
+    // End of variables declaration//GEN-END:variables
+
+}
